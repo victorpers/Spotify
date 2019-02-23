c830ffaf0545a757b100a6caf02452cf47cba987
Victor Pers <victorpers2@gmail.com>
2019-02-23 15:10:09 +0100

    Connecting to our database from PHP

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php
+
+	ob_start();
+
+	$timezone = date_default_timezone_set("Europe/Paris");
+
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=spotify;charset=utf8', 'root', '');
+	}
+	catch (Exception $e)
+	{
+	        die('Erreur : ' . $e->getMessage());
+	}
+
+?>
