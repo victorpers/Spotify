5b54b70042d4c659c0f76db158705c57683aed6a
Victor Pers <victorpers2@gmail.com>
2019-02-24 11:17:05 +0100

    Sessions

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
+<?php
+include("includes/config.php");
+
+//session_destroy(); Log out manually
+
+if (isset($_SESSION['userLoggedIn'])) {
+	$userLoggedIn = $_SESSION['userLoggedIn'];
+}
+else {
+	header("Location: register.php");
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Spotify</title>
+	<title>Bienvenue sur Spotify</title>
 	<meta charset="utf-8">
 </head>
 <body>
-	<h1 style="text-align: center;">Hello</h1>
+	<h1 style="text-align: center;">Spotify</h1>
 
 </body>
 </html>
